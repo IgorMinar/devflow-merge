c1192d41ea5f656e1145cc8d789299ee3ba0765d
Igor Minar <igor@angularjs.org>
2016-09-30 21:41:23 +0100

    fix: make frame longer

--- source.ts
+++ source.ts
@@ -1,5 +1,5 @@
-=======================
+==============================
 Cool stuff right here!
 And more stuff right here!
 Totally new thing right here!
-=======================
+==============================
